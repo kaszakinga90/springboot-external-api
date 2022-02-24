9a5231934bfae14ef59bb934c94a62100f96ed30
kaszakinga90 <kaszakinga90@gmail.com>
2022-02-24 14:06:27 +0100

    first commit

---
+++ src/test/java/pl/projekty/springexternalapi/SpringExternalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.projekty.springexternalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExternalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
